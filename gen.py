eea112575e9c88341ddef15f4158b84cfdc0d4a9
rolinmb <rolin.blake@gmail.com>
2021-09-09 22:49:45 -0500

    second

--- gen.py
+++ gen.py
@@ -1,12 +1,13 @@
 import time
 import subprocess
 import soundfile as sf
-from Pedalboard import (
-    Pedalboard, Chorus, Phaser
-)
+from Pedalboard import *
 
 def convert_wav(inmp3,outwav):
     subprocess.call(['ffmpeg','-i',inmp3,outwav])
 
 if __name__ == '__main__':
-    
+    start = time.time()
+    wav,sr = sf.read('final_boass.wav')
+    fx = Pedalboard([LadderFilter(mode=LadderFilter.Mode.HPF12,cutoff_hz=900)])
+    print('Program Execution Time: '+str(reound(time.time()-start,2))+' seconds.')
