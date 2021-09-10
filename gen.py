57c5f50d25cc1b221dc18f541f338e06930e4655
rolinmb <rolin.blake@gmail.com>
2021-09-10 13:18:06 -0500

    new computer

--- gen.py
+++ gen.py
@@ -1,13 +1,35 @@
-import time
-import subprocess
 import soundfile as sf
-from Pedalboard import *
+import subprocess
+import time
+from pedalboard import *
 
-def convert_wav(inmp3,outwav):
-    subprocess.call(['ffmpeg','-i',inmp3,outwav])
+def to_wav(mp3,wavOut):
+	print('Converting '+mp3+' to '+wavOut)
+	subprocess.call(['ffmpeg','-i',mp3,wavOut],shell=True)
 
+def getBoard(rate):
+	pb = Pedalboard([
+		Chorus(rate_hz=12.5,depth=0.15,mix=0.4),
+		Phaser(rate_hz=0.75,depth=0.1,mix=0.175)
+	],sample_rate=rate)
+	return pb
+	
+def writeFx(fx,wav,osr,dest):
+	result = fx(wav)
+	print('Output Sample Rate: '+str(osr)+' Hz')
+	print('\t-> Writing effected audio to \''+dest+'\'')
+	with sf.SoundFile(dest,'w',samplerate=osr,channels=len(result.shape)) as f:
+		f.write(result)
+	
 if __name__ == '__main__':
-    start = time.time()
-    wav,sr = sf.read('final_boass.wav')
-    fx = Pedalboard([LadderFilter(mode=LadderFilter.Mode.HPF12,cutoff_hz=900)])
-    print('Program Execution Time: '+str(reound(time.time()-start,2))+' seconds.')
+	start = time.time()
+	inName = 'sample.wav'
+	outName = 'out.wav'
+	#to_wav('boass.mp3',inName)
+	wav,inRate  = sf.read(inName) 
+	print('Input Sample Rate:'+str(inRate)+' Hz')
+	outRate = 44100 #44100 Hz is Standard
+	board =getBoard(outRate)
+	outName = 'out.wav'
+	writeFx(board,wav,outRate,outName)
+	print('Total Execution Time: '+str(round(time.time()-start,2))+' seconds.')
